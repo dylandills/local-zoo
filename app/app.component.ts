e5bf293ac293bfa81eb6470f760bf0a12da46914
Dylan Dills <dylan.dills@gmail.com>
2017-07-07 09:34:51 -0700

    inital commit

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class="container">
+    <h1>Local Zoo!</h1>
+  </div>
+  `
+})
+
+export class AppComponent {
+
+}
